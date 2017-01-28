7dd0b1ec1abf7b4f89cdbc0c6f4ebcef49905800
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-01-28 21:11:28 +0700

    add route '/paypal'

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const {Router} = require('express')
 const transactions = require('./transactions')
+const paypal = require('./paypal')
 
 const router = Router()
 router.use('/transactions', transactions)
+router.use('/paypal', paypal)
 
 module.exports = router
