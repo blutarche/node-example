c9abd48c74b6f4c242808e929ee2a438a6782622
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-01-28 21:01:07 +0700

    require paypal-sdk and refer all credentials to other files

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const expressValidator = require('express-validator')
+const paypal = require('paypal-rest-sdk')
+const port = process.env.PORT || 3000;
+const configDB = require('./credentials/mongolab.js');
+const paypalCredential = require('./credentials/paypal.js');
 
 const app = express()
 
-mongoose.connect('mongodb://test01:qwerty@ds129469.mlab.com:29469/node-test')
+mongoose.connect(configDB.url)
+paypal.configure({
+	'mode': 'sandbox',
+	'client_id': paypalCredential.client_id,
+	'client_secret': paypalCredential.client_secret 
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -14,4 +23,4 @@ app.use(expressValidator());
 app.use('/', routes)
 
 
-app.listen('3000', () => console.log('listen in port 3000'))
+app.listen(port, () => console.log('listen in port '+port))
